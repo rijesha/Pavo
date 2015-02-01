20b4e273a5e86e78c5432241f549a5bcc792e8c4
Robert Sipho <robert.sipho@gmail.com>
2015-02-01 01:50:34 -0700

    math servo init file

---
+++ earthMath.cpp
@@ -0,0 +1,24 @@
+void initservo(){
+	/*runs once at start*/
+	/*points the platform at magnectic north,
+	change later to incorporate angle of declination*/
+	initializeallvalues();
+	if (lat >= 0){
+		if (direc<90 && direc >= 0){
+			servomove(1, direc);
+		}
+		else if (direc<0 && direc>-90){
+			int intmv = 0;
+			intmv = abs(direc) + 90;
+			servomove(1, intmv);
+		}
+		else if (lat<0){
+
+		}
+	}
+	/*initialise servo 2*/
+	int abslat = 90 * sin(2 * pi*abs(lat) / 180);
+
+	servomove(2, abslat);
+
+}
