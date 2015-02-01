039d65b789f5b825cf7acf176507a571ec3d636e
Robert Sipho <robert.sipho@gmail.com>
2015-02-01 02:04:59 -0700

    Southern hemisher math

--- earthMath.cpp
+++ earthMath.cpp
@@ -1,9 +1,25 @@
+#include <math.h>
+
 void initservo(){
 	/*runs once at start*/
 	/*points the platform at magnectic north,
 	change later to incorporate angle of declination*/
 	initializeallvalues();
 	if (lat >= 0){
+		if (direc<90 && direc >= 0){
+			servomove(1, direc);
+		}
+		else if (direc<0 && direc>-90){
+			int intmv = 0;
+			intmv = abs(direc) + 90;
+			servomove(1, intmv);
+		}
+		else if (lat<0){
+
+		}
+	}
+	else if (lat < 0){
+		float newdirac = 180 - dirac;
 		if (direc<90 && direc >= 0){
 			servomove(1, direc);
 		}
